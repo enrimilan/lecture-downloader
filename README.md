3a64d8a440a6d9318f791b3bd9ee4c9cd05704ed
Enri Miho <e0929003@student.tuwien.ac.at>
2017-07-16 00:21:00 +0200

    init repository

---
+++ README.md
@@ -0,0 +1,2 @@
+## Lecture downloader
+The Lecture downloader is an extension for Google Chrome. It can download lecture videos from opencast matternhorn systems.
---
+++ popup.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.getSelected(null, function(tab) {
+        
+        // parse opencast host and id of the lecture, may be adapted according to your needs
+        var url = tab.url;
+        var host = "https://mh-engage.ltcc.tuwien.ac.at";
+        var id = getParameterByName('id', url);
+        if(host == null || id == null) return;
+     
+        // get download url and lecture title
+        var http = new XMLHttpRequest();
+        var params = "id="+id;
+        http.open("GET", host+"/search/episode.xml?"+params, true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(http.responseText, "text/xml");
+                var title = xmlDoc.getElementsByTagName("title")[0].innerHTML;
+                if (title == null) title = "video"
+                var videoUrl = xmlDoc.getElementsByTagName("url")[0].innerHTML;
+                if (videoUrl == null) return;
+                
+                var downloadButton = document.getElementById('downloadButton');
+                downloadButton.disabled = false;
+                downloadButton.addEventListener('click', function() {
+                    chrome.downloads.download({
+  	                     url: videoUrl,
+  	                     filename: title + ".flv"
+                    });
+                });
+            }
+        }
+        http.send(null);
+    }); 
+});
+
+// thanks to https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
